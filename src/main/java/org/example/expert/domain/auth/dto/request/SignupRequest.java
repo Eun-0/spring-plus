b23148afe92b5d2f4d2745648546ee896252e7ec
eun0 <shinely12@gmail.com>
2024-11-15 16:32:18 +0900

    [Level 1-2] 코드 추가 퀴즈 - JWT 의 이해
    - [Bean Validation] SignUpRequest DTO 의 nickname 조건
    
    - nickname 조건
      - 최소 1자에서 최대 16자
      - 한글, 영어, 숫자, 하나의 공백만 허용
      - 연속된 공백 두 개 이상은 비허용

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,4 +18,12 @@ public class SignupRequest {
     private String password;
     @NotBlank
     private String userRole;
+
+    // nickname 조건
+    // 최소 1자에서 최대 16자
+    // 한글, 영어, 숫자, 하나의 공백만 허용
+    // 연속된 공백 두 개 이상은 비허용
+    @NotBlank   // null 과 ""(초기화된 String), " "(공백) 모두 비허용
+    @Pattern(regexp = "^(?!.*\\s{2,})[a-zA-Z0-9가-힣\\s]{1,16}$", message = "닉네임 형식이 올바르지 않습니다.")
+    private String nickname;
 }
