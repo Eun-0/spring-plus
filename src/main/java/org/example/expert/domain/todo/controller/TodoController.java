cf106c364fe9fc8dc9d48ef64b89e5162a1c8f2c
eun0 <shinely12@gmail.com>
2024-11-19 00:43:16 +0900

    [Level 1-5] 코드 개선 퀴즈 - JPA 의 이해
    - 할 일 검색 시, 'weather' 조건과 '수정일 기준의 기간'으로 검색 가능
    - 조건은 있을 수도 있고, 없을 수도 있음
      - weather 조건 : String 형식
      - 수정일 기준의 기간 조건의 시작일(startDate) : 'yyyy-MM-ddT00:00:00' LocalDateTime 형식
      - 수정일 기준의 기간 조건의 종료일(endDate) : 'yyyy-MM-ddT23:59:59.999999999' LocalDateTime 형식

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
@@ -26,12 +28,21 @@ public class TodoController {
         return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
     }
 
+    // 기획자의 긴급 요청
+    // 할 일 검색 시, 'weather' 조건과 '수정일 기준의 기간'으로 검색이 가능해야 함
+    // 조건은 있을 수도 있고, 없을 수도 있음
+    // - weather 조건 : String 형식으로 입력됨
+    // - 수정일 기준의 기간 조건의 시작일(startDate) : 'yyyy-MM-ddT00:00:00' LocalDateTime 형식으로 입력됨
+    // - 수정일 기준의 기간 조건의 종료일(endDate) : 'yyyy-MM-ddT23:59:59.999999999' LocalDateTime 형식으로 입력됨
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String weather,
+            @RequestParam(required = false) LocalDateTime startDate,
+            @RequestParam(required = false) LocalDateTime endDate
     ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+        return ResponseEntity.ok(todoService.getTodos(page, size, weather, startDate, endDate));
     }
 
     @GetMapping("/todos/{todoId}")
