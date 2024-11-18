fb5583e8e49990218c21ad61b075cfc51c2db122
eun0 <shinely12@gmail.com>
2024-11-19 01:08:21 +0900

    [Level 1-5] 코드 개선 퀴즈 - JPA 의 이해
    retrieveByConditionOrderByModifiedAtDesc 메서드
    - 'weather' 조건과 '수정일 기준의 기간'으로 검색
      - 이때, 조건은 있을 수도 있고 없을 수도 있음
    - 수정일 기준으로 내림차순 정렬

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
+    // 'weather' 조건과 '수정일 기준의 기간'으로 검색 & 수정일 기준 내림차순 정렬
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user u " +
+            "WHERE (:weather IS NULL OR t.weather LIKE %:weather%) AND " +
+            "(:startDate IS NULL OR t.modifiedAt >= :startDate) AND " +
+            "(:endDate IS NULL OR t.modifiedAt <= :endDate) " +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> retrieveByConditionOrderByModifiedAtDesc(
+            @Param("weather") String weather,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable
+    );
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
