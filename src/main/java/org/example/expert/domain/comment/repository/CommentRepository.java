060af3ad53f94d9fdaebe9c90401b3fa5527d191
eun0 <shinely12@gmail.com>
2024-11-21 00:28:41 +0900

    [Level 2-7] N+1 문제
    해결 방법
    1. Fetch Join
    2. @EntityGraph
    3. @NamedEntityGraph -> Comment Entity 에도 설정

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.comment.repository;
 
 import org.example.expert.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,22 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+//    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+//    List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+
+    // 1. Fetch Join
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+
+    // 2. @EntityGraph
+//    @EntityGraph(attributePaths = {"user"})
+//    @EntityGraph(attributePaths = {"user"}, type = EntityGraph.EntityGraphType.LOAD)
+//    @Query("SELECT c FROM Comment c WHERE c.todo.id = :todoId")
+//    List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+
+    // 3. @NamedEntityGraph
+//    @EntityGraph("Comment.findAll")
+//    @EntityGraph(value = "Comment.findAll", type = EntityGraph.EntityGraphType.LOAD)
+//    @Query("SELECT c FROM Comment c WHERE c.todo.id = :todoId")
+//    List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
