da32ad34d2f281abfb77407157dffb4de55fe613
eun0 <shinely12@gmail.com>
2024-11-21 11:57:37 +0900

    [Level 2-8] QueryDSL
    - TodoRepositoryCustomImpl
      - 사용자 정의 인터페이스 TodoRepositoryCustom 구현체

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryCustomImpl.java
@@ -0,0 +1,30 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.example.expert.domain.todo.entity.QTodo;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.QUser;
+
+import java.util.Optional;
+
+public class TodoRepositoryCustomImpl implements TodoRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public TodoRepositoryCustomImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId){
+        QTodo todo = QTodo.todo;
+        QUser user = QUser.user;
+
+        return Optional.ofNullable(queryFactory
+                .selectFrom(todo)
+                .leftJoin(todo.user, user)
+                .where(todo.id.eq(todoId))
+                .fetchOne());
+    }
+}
